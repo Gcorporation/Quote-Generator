38135d433a508d996b0a676bb764aff0c94579a8
Prabhjot Singh <prabhjotsingh394@gmail.com>
2022-06-23 15:57:45 +0530

    Manipulating DOM with JS

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-button');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
+
+// generate random number to get random quote
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -18,4 +38,19 @@ async function getQuotes() {
     }
 }
 
+// On Load
+
 getQuotes();
+
+
+// Tweet Quote
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// add EventListeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
